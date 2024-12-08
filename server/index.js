aafe48995830810bbf41ac54a17b52d9cdc985fa
zakizaidi55 <zakizaidi55@gmail.com>
2024-12-08 12:24:42 +0530

    .env example file and change backend url

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://study-notion-two-phi.vercel.app",
+		origin:"http://localhost:3000",
 		credentials:true,
 	})
 )
