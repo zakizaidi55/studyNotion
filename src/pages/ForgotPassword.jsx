c6e215f69c6446f141e68b1625fa9035a71ac2f2
zakizaidi55 <zakizaidi55@gmail.com>
2025-06-23 12:21:39 +0530

    Add Forgot password UI

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -18,66 +18,55 @@ function ForgotPassword() {
     }
 
   return (
-    <div className='text-white'>
-        {
-            loading ? (
-                <div className='flex justify-center items-center text-richblack-5 font-bold text-3xl'>
-                    ...Loading
-                </div>
-               
-            ) : 
-            (
-                <div>
-                    <h1>
-                        {
-                            !emailSent ? "Reset your password" : "Check your Email"
-                        }
-                    </h1>
-
-                    <p>
-                        {
-                            !emailSent ? "Have no fear. We'll email you instructions to reset your password. If you dont have access to your email we can try account recovery" 
-                            : `We have sent the reset email to ${email}`
-                        }
-                    </p>
-
-                    <form onSubmit={handleOnSubmit}>
-                        {
-                            !emailSent && (
-                                <label>
-                                    <p>Email Address*</p>
-                                    <input
-                                        
-                                        required
-                                        type="email"
-                                        name="email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        placeholder="Enter email address"
-                                        className="form-style w-full text-richblue-900"
-                                    />
-                                </label>
-                            )
-                        }
-
-                        <button type='submit'>
-                            {
-                                !emailSent ? "Reset password" : "Sent Email"
-                            }
-                        </button>
-                    </form>
-
-                    <div>
-                        <Link to="/login">
-                            <p>Back to Login</p>
-                        </Link>
-                    </div>
-
-                </div>
-            )
-        }
+    <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+      {loading ? (
+        <div className="spinner"></div>
+      ) : (
+        <div className="max-w-[500px] p-4 lg:p-8">
+          <h1 className="text-[1.875rem] font-semibold leading-[2.375rem] text-richblack-5">
+            {!emailSent ? "Reset your password" : "Check email"}
+          </h1>
+          <p className="my-4 text-[1.125rem] leading-[1.625rem] text-richblack-100">
+            {!emailSent
+              ? "Have no fear. We'll email you instructions to reset your password. If you dont have access to your email we can try account recovery"
+              : `We have sent the reset email to ${email}`}
+          </p>
+          <form onSubmit={handleOnSubmit}>
+            {!emailSent && (
+              <label className="w-full">
+                <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+                  Email Address <sup className="text-pink-200">*</sup>
+                </p>
+                <input
+                  required
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter email address"
+                  className="form-style w-full"
+                />
+              </label>
+            )}
+            <button
+              type="submit"
+              className="mt-6 w-full rounded-[8px] bg-yellow-50 py-[12px] px-[12px] font-medium text-richblack-900"
+            >
+              {!emailSent ? "Sumbit" : "Resend Email"}
+            </button>
+          </form>
+          <div className="mt-6 flex items-center justify-between">
+            <Link to="/login">
+              <p className="flex items-center gap-x-2 text-richblack-5">
+                <BiArrowBack /> Back To Login
+              </p>
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
+
 
 export default ForgotPassword
